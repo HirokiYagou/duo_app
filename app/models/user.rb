2040dd17f524011e06e337c8283f81babbc35237
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-22 17:46:39 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  USERNAME_REGEX = /\A[a-z_0-9]+\z/.freeze
+  EMAIL_REGEX = /\A\S+@\S+\.\S+\z/.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  FULLNAME_REGEX = /\A[a-z]+\z/.freeze
+  with_options presence: true do
+    validates :username, format: { with: USERNAME_REGEX, message: 'は小文字の英字、数字、アンダーバーを入力してください'}
+    validates :email, format: { with: EMAIL_REGEX,    message: 'には@を含めてください' }
+    validates :password, format: { with: PASSWORD_REGEX, message: 'には半角で英字と数字の両方を含めて設定してください' }
+    with_options format { with: FULLNAME_REGEX, message: 'は小文字の英字で入力してください'} do
+      validates :last_name
+      validates :first_name
+    end
+  end
 end
