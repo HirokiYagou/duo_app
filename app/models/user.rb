b9b1a6a003cbeaa2ae7522d311c6f3765e914a2a
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-22 20:58:17 +0900

    userモデル単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  USERNAME_REGEX = /\A[a-z_0-9]+\z/.freeze
+  USERNAME_REGEX = /\A[a-zA-Z_0-9]+\z/.freeze
   EMAIL_REGEX = /\A\S+@\S+\.\S+\z/.freeze
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   FULLNAME_REGEX = /\A[a-zA-Z]+\z/.freeze
   with_options presence: true do
-    with_options uniqueness: true do
-      validates :username, format: { with: USERNAME_REGEX, message: 'は小文字の英字、数字、アンダーバーを入力してください'}
+    with_options uniqueness: { case_sensitive: false } do
+      validates :username, format: { with: USERNAME_REGEX, message: 'は英字、数字、アンダーバーを入力してください'}
       validates :email, format: { with: EMAIL_REGEX,    message: 'には@を含めてください' }
     end
     validates :password, format: { with: PASSWORD_REGEX, message: 'には半角で英字と数字の両方を含めて設定してください' }
