4f7bfba3fc15cf0d47baf93b14d7226d2ada9c6b
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-23 00:03:56 +0900

    Update webpacker.yml

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import { createApp } from 'vue';
-import Posts from 'posts/index.vue';
+import { createApp } from "vue"
+import Posts from "posts/index.vue"
 
 document.addEventListener('DOMContentLoaded', () => {
   createApp(Posts).mount('#posts')
--- app/javascript/posts/index.vue
+++ app/javascript/posts/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <p>{{ message }}</p>
+<div>
+  <button class="button" @click="onClick">投稿する</button>
+  
+  <div :class="['modal', isActive]">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div class="columns is-mobile is-centered">
+        <div class="column is-half">
+          <p>{{ message }}</p>
+        </div>
+      </div>
+    </div>
+    <button class="modal-close is-large" aria-label="close"></button>
+  </div>
+</div>
 </template>
 
 <script>
@@ -7,7 +21,13 @@ export default {
   data() {
     return {
       message: 'たまプラーザ',
+      isActive: '',
     }
+  },
+  methods: {
+    onClick: function() {
+      this.isActive = 'is-active'
+    },
   },
 }
 </script>
