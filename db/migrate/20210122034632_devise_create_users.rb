6ca151315f3b36f787dffa86691fa3f4fde251f7
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-22 13:51:46 +0900

    マイグレーション

--- db/migrate/20210122034632_devise_create_users.rb
+++ db/migrate/20210122034632_devise_create_users.rb
@@ -4,8 +4,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
 
       ## Recoverable
       t.string   :reset_password_token
