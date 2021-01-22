4802a1806672b649a864f657f751821e73f43b06
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-22 19:18:20 +0900

    Update seeds.rb

--- db/seeds.rb
+++
@@ -1,8 +0,0 @@
-User.create!(
-  username:  "admin",
-  email: "admin@gmail.com",
-  password:  "1qaz2wsx",
-  password_confirmation: "1qaz2wsx",
-  last_name: "yago",
-  first_name: "hiroki",
-  admin: true)
