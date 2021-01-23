de7b236dc71085d1c5669c0393e9d009efb9d32b
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-23 13:35:30 +0900

    postモデル単体テスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :post do
-    
+    content { Faker::Movies::HarryPotter.quote }
+    association :user
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/images/icon11.png'), filename: 'icon11.png')
+    end
   end
+
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+  describe "新規投稿機能"
+    context '新規投稿ができるとき' do
+      it 'フォームが全て埋まっていれば投稿できる' do
+        expect(@post).to be_valid
+      end
+    end
+    context '新規投稿ができないとき' do
+      it '本文が空では投稿できない' do
+        @post.content = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Content can't be blank")
+      end
+      it '本文が140字を超えると投稿できない' do
+        text = ''
+        141.times do
+          text << 'a'
+        end
+        @post.content = text
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Content is too long (maximum is 140 characters)")
+      end
+    end
 end
