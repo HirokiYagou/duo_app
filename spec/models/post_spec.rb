e01aaee1cde87cf51ff5d1bd320a9899d6a12743
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-23 23:41:10 +0900

    Update post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Post, type: :model do
   before do
     @post = FactoryBot.build(:post)
   end
-  describe "新規投稿機能"
+  describe "新規投稿機能" do
     context '新規投稿ができるとき' do
       it 'フォームが全て埋まっていれば投稿できる' do
         expect(@post).to be_valid
@@ -26,4 +26,5 @@ RSpec.describe Post, type: :model do
         expect(@post.errors.full_messages).to include("Content is too long (maximum is 140 characters)")
       end
     end
+  end
 end
