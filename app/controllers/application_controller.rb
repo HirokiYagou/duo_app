2e67bd759c6904ca132d2fd233eba95a848a4b9d
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-23 16:00:01 +0900

    Revert "本番環境にadmin実装準備"
    This reverts commit 888eb27718d75e9d1ec44032e700d49f612597bb.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :last_name, :first_name, :admin])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :last_name, :first_name])
   end
 end
