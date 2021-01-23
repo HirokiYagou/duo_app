274558b7eb79eafc36116da664e24e12499afd00
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-23 16:56:48 +0900

    dropdown導入

--- app/javascript/posts/posts.vue
+++ app/javascript/posts/posts.vue
@@ -21,6 +21,47 @@
           </time>
         </p>
       </div>
+      <div class="media-right">
+        <div class="dropdown">
+          <div class="dropdown-trigger">
+            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
+              <span class="icon is-small">
+                <img src="/assets/bars_24.png" alt="">
+                <i class="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </div>
+          <div class="dropdown-menu" id="dropdown-menu3" role="menu">
+            <div class="dropdown-content">
+              <a href="#" class="dropdown-item">
+                Overview
+              </a>
+              <a href="#" class="dropdown-item">
+                Modifiers
+              </a>
+              <a href="#" class="dropdown-item">
+                Grid
+              </a>
+              <a href="#" class="dropdown-item">
+                Form
+              </a>
+              <a href="#" class="dropdown-item">
+                Elements
+              </a>
+              <a href="#" class="dropdown-item">
+                Components
+              </a>
+              <a href="#" class="dropdown-item">
+                Layout
+              </a>
+              <hr class="dropdown-divider">
+              <a href="#" class="dropdown-item">
+                More
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
     <div class="content">
@@ -49,5 +90,8 @@ export default {
 .content> p {
   white-space: pre-wrap;
   word-wrap: break-word;
+}
+.dropdown button {
+  border: none;
 }
 </style>
