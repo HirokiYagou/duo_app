ef55892a0db42e81b119c143d7c403514a7349a4
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-24 18:59:37 +0900

    new&create_profileルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user, do
+    get 'profiles', to: 'users/registrations#new_profile'
+    post 'profiles', to: 'users/registrations#create_profile'
+  end
   root to: 'posts#index'
   resources 'posts', only: [:index, :create, :update, :destroy]
   namespace :admin do
