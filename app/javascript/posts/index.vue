64ff5ba229dcf4decf691162a5aa8d1be2950c39
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-24 13:36:35 +0900

    サイドバーをコンポーネント化

--- app/javascript/posts/index.vue
+++ app/javascript/posts/index.vue
@@ -14,15 +14,17 @@
 
   <div class="columns left-menu">
     <div class="column is-one-fifth">
-      <a href="/users/sign_out" data-method="delete">ログアウト</a>
-      <p>{{ currentuser.name }}</p>
-      <button class="button is-primary is-fullwidth" @click="openForm">投稿する</button>
+      <left-bar
+        :current_user_name="currentUser.name"
+        @do-open-form="openForm"
+        @do-set-user-posts="setUserPosts(currentUser.name)"
+      ></left-bar>
     </div>
     <div class="column is-two-thirds">
       <div v-for="(post, index) in templatePosts" :key="post.id" :data-id="post.id" class="card">
         <post
           :post="post"
-          :current_user_name="currentuser.name"
+          :current_user_name="currentUser.name"
           @delete-post="deletePost($event, index)"
           @edit-post="editPost($event, index)"
           @open-img-modal="openImgModal($event)"
@@ -35,6 +37,7 @@
 </template>
 
 <script>
+import LeftBar from './left_bar'
 import Form from './form'
 import Image from './img_modal'
 import Post from './post'
@@ -42,13 +45,14 @@ import { csrfToken } from "@rails/ujs"
 
 export default {
   components: {
+    'left-bar': LeftBar,
     'post-form': Form,
     'img-modal': Image,
-    'post': Post
+    'post': Post,
   },
   data() {
     return {
-      currentuser: {},
+      currentUser: {},
       allPosts: [],
       templatePosts: [],
       post: {},
@@ -84,7 +88,7 @@ export default {
         .then(data => {
           this.allPosts = data.posts
           this.templatePosts = this.allPosts
-          this.currentuser = data.currentuser
+          this.currentUser = data.currentuser
         })
         .catch(error => {
           console.log(error)
---
+++ app/javascript/posts/left_bar.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="left-bar-wrapper">
+  <a class="button is-fullwidth" href="/users/sign_out" data-method="delete">ログアウト</a>
+  <button class="button is-fullwidth" @click="doSetUserPosts">{{ current_user_name }}</button>
+  <button class="button is-primary is-fullwidth" @click="doOpenForm">投稿する</button>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    current_user_name: String,
+  },
+  methods: {
+    doOpenForm: function() {
+      this.$emit("do-open-form")
+    },
+    doSetUserPosts: function() {
+      this.$emit("do-set-user-posts")
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* .left-bar-wrapper {
+  position: fixed;
+} */
+</style>
