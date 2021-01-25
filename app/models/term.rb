78488e4add30ff82c9f367d30add090e6eb64fc1
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-25 19:26:31 +0900

    termモデルバリデーション

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,2 +1,9 @@
 class Term < ApplicationRecord
+  with_options presence: true do
+    validates :sentence_id, format: { with: /\A[0-9]+\z/.freeze, message: 'は半角数字を入力してください' }
+    validates :word_id, format: { with: /\A[0-9]+\z/.freeze, message: 'は半角数字を入力してください' }
+    validates :lesson
+    validates :english
+    validates :japanese
+  end
 end
