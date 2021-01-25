2914ada8f0b4574de50004f133fb8d0189ce214a
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-25 16:07:29 +0900

    termモデル生成

---
+++ db/migrate/20210125070503_create_terms.rb
@@ -0,0 +1,11 @@
+class CreateTerms < ActiveRecord::Migration[6.0]
+  def change
+    create_table :terms do |t|
+      t.integer :term_type, null: false
+      t.integer :lesson, null: false
+      t.text :english, null: false
+      t.text :japanese, null: false
+      t.timestamps
+    end
+  end
+end
