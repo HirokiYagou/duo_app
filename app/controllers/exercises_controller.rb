4345f30bffb3ea69739ae71feac24d1172fc66bb
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-26 16:39:34 +0900

    exercises#updateアクションルーティング

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,6 +2,10 @@ class ExercisesController < ApplicationController
   def index
   end
 
+  def update
+    
+  end
+  
   def get_sentences
     questions = Term.where(lesson: params[:id]).where(word_id: 0)
     render json: { questions: questions }
