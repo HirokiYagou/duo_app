61bce8f439e9ddf8637b168099d4c596dfece19d
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-28 20:19:12 +0900

    postが削除された時に紐づくfavoriteも削除される実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
 
   validates :content, presence: true, length: { maximum: 140 }
 end
