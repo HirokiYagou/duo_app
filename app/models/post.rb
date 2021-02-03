1cbf13a61f46e01915d33b7b0c3d9ba34f36484b
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-02-04 01:49:19 +0900

    post_termsに対してdependent: :destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :favorites, dependent: :destroy
-  has_many :post_terms
+  has_many :post_terms, dependent: :destroy
   has_many :terms, through: :post_terms
 
   validates :content, presence: true, length: { maximum: 140 }
