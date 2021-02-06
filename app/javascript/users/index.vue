65d9feceeec0f4d345565460ffefc791f6a049d1
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-02-06 14:53:12 +0900

    tophero導入

--- app/javascript/users/index.vue
+++ app/javascript/users/index.vue
@@ -1,14 +1,15 @@
 <template>
 <div>
-  <nav class="navbar">
+  <nav class="navbar is-fixed-top">
     <div class="container">
       <div id="navMenu" class="navbar-menu">
         <div class="navbar-start">
-          <a class="navbar-item">
-            Home
+          <img src="/assets/duo3.jpg" alt="" class="navbar-item">
+          <a class="navbar-item" @click="goToPost">
+            POST
           </a>
-          <a class="navbar-item">
-            Documentation
+          <a class="navbar-item" @click="goToExercise">
+            EXERCISE
           </a>
         </div>
 
@@ -22,6 +23,14 @@
       </div>
     </div>
   </nav>
+  <section class="navbar-back">navbar-back</section>
+
+  <section class="hero is-light is-large">
+    <div class="hero-body">
+      <p class="title has-text-centered">Enjoy Yourself with DUO3.0!!</p>
+    </div>
+  </section>
+
 
   <section class="hero is-primary">
     <div class="hero-body">
@@ -48,7 +57,9 @@
 </template>
 
 <script>
+import section from '../exercises/section.vue'
 export default {
+  components: { section },
   methods: {
     goToPost: function() {
       window.location.href = '/posts'
@@ -61,10 +72,13 @@ export default {
 </script>
 
 <style scoped>
-.hero {
-  height: 45vh;
-}
 .navbar {
   height: 10vh;
+}
+.navbar-back {
+  height: 10vh;
+}
+.top-hero {
+  height: 80vh;
 }
 </style>
