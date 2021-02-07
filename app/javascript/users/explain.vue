9d4f57fb3758d46b6893e18cdb8d2de8d0df1bfa
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-02-07 14:14:21 +0900

    section内要素とレイアウト決定

--- app/javascript/users/explain.vue
+++ app/javascript/users/explain.vue
@@ -1,9 +1,12 @@
 <template>
 <section class="section">
-  <h1 class="title">Section</h1>
-  <h2 class="subtitle">
-    A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
-  </h2>
+  <h4 class="title is-4">単語帳の完成形</h4>
+  <p class="is-size-5">
+    学習していて楽しい<br>
+    効率の良い学習ができる<br>
+    使い方は無限大！<br>
+    そんなDUOの魅力をお伝えします
+  </p>
 </section>
 </template>
 
@@ -35,5 +38,6 @@ export default {
 <style scoped>
 .section {
   border: 1px solid black;
+  height: 30vh;
 }
 </style>
