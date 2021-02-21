81a9588bf65a75ba61d75f158399af46d810b69c
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-02-21 15:04:54 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.15.0'
 set :application, 'duo_app'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:https://github.com/HirokiYagou/duo_app.git'
+set :repo_url,  'git@github.com:HirokiYagou/duo_app.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
