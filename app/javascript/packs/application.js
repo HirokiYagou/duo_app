94ee8863dee35b67132bc0e861ed589f425ff849
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-03-03 18:36:09 +0900

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,13 +4,24 @@ require("@rails/activestorage").start()
 require("channels")
 
 import { createApp } from "vue"
+import { createRouter, createWebHashHistory } from "vue-router"
 import Users from "users/index"
 import Posts from "posts/index"
 import Exercises from "exercises/index"
 import Terms from "admin/terms/index"
 
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    { path: '/', component: Users}
+  ]
+})
+
 document.addEventListener('DOMContentLoaded', () => {
-  createApp(Users).mount('#users')
+  const users = createApp(Users)
+  users.use(router)
+  users.mount('#users')
+  // createApp(Users).mount('#users')
   createApp(Posts).mount('#posts')
   createApp(Exercises).mount('#exercises')
   createApp(Terms).mount('#terms')
